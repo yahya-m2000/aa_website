9b4352f5a3c9f790df4cfe675409682f7d388de7
Yahya Mohamed <yahyaimohamed2000@gmail.com>
2025-12-29 17:49:29 +0000

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -43,6 +44,7 @@ export default function RootLayout({
     >
       <body className="antialiased bg-white text-slate-900">
         {children}
+        <Analytics />
       </body>
     </html>
   );
